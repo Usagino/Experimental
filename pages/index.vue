aac7a5d64ae939666d7b9b1de38e4ab7a1b11aeb
yuki <yuki.itoh@front-artgraph.com>
2019-10-26 08:48:10 +0900

    ページをスクロールするとsvgのマスクアニメーションが発火

--- pages/index.vue
+++ pages/index.vue
@@ -5,14 +5,24 @@
         nuxt-link(to="/boids") boids
       li
         nuxt-link(to="/d3force") d3 force
+      li
+        nuxt-link(to="/scroll_svg_change") scroll SVG change
 </template>
 
 <script>
 import Logo from '~/components/Logo.vue'
 
+
+
+
+
+
 export default {
   components: {
     Logo
+  },
+  mounted(){
+    console.log("hi");
   }
 }
 </script>
